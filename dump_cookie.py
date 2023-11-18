cfc9b7d89683b560225db6ca51ceca4b84747ea1
Timothy <143961127+TimkaSR1@users.noreply.github.com>
2023-11-18 16:55:58 +0200

    Create dump_cookie.py

---
+++ dump_cookie.py
@@ -0,0 +1,25 @@
+import time, pickle
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+chrome_driver_path = 'YouDontNeedToChangeThisBtw'
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.get('https://twitter.com/i/flow/login') # Opens Twitter
+time.sleep(2)
+email = driver.find_element('name', 'text')
+email.send_keys('') # Email
+email.send_keys(Keys.ENTER)
+time.sleep(2)
+recheck = driver.find_element('name', 'text')
+recheck.send_keys('') # Username in case it requires it
+recheck.send_keys(Keys.ENTER)
+time.sleep(2)
+password = driver.find_element('name', 'password')
+password.send_keys('') # Password
+password.send_keys(Keys.ENTER)
+time.sleep(2)
+pickle.dump( driver.get_cookies() , open("cookie.pkl","wb")) # Dumps the cookie
+time.sleep(2)
+driver.quit()
