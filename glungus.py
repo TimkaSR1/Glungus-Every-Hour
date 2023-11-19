8714fd08c2f29b15040675e4a476ba90c887f15f
Timothy <143961127+TimkaSR1@users.noreply.github.com>
2023-11-19 22:03:02 +0200

    Update glungus.py

--- glungus.py
+++ glungus.py
@@ -14,7 +14,7 @@ def load_cookies(driver, location, url=None):
         driver.add_cookie(cookie)
 
 def tweet():
-	chrome_driver_path = 'YouDontNeedToChangeThisBtw'
+	chrome_driver_path = '' # Enter the path of the chromedriver
 driver = webdriver.Chrome()
 driver.maximize_window()
 load_cookies(driver, "cookie.pkl") # Imports the cookie you dumped earlier
