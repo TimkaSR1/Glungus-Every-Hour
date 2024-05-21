e80e01e7bd81c36e00d8b964d0c13231149a65bb
Timothy <143961127+TimkaSR1@users.noreply.github.com>
2024-05-21 12:19:45 +0300

    Update dump_cookie.py
    Domain change :crying_cat_face:

--- dump_cookie.py
+++ dump_cookie.py
@@ -7,7 +7,7 @@ chrome_driver_path = '' # Enter the path of the chromedriver
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-driver.get('https://twitter.com/i/flow/login') # Opens Twitter
+driver.get('https://x.com/i/flow/login') # Opens Twitter
 time.sleep(2)
 email = driver.find_element('name', 'text')
 email.send_keys('') # Email
